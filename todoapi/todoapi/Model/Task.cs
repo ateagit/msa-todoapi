928d72c713b7e080ad1a3365c0f5216d22927087
Alan <alanunny@outlook.com>
2019-06-27 14:04:07 +1200

    first commit

---
+++ todoapi/todoapi/Model/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace todoapi.Model
+{
+    public partial class Task
+    {
+        public int TaskId { get; set; }
+        [StringLength(255)]
+        public string TaskName { get; set; }
+        [Column(TypeName = "date")]
+        public DateTime? StartDate { get; set; }
+        [Column(TypeName = "date")]
+        public DateTime? DueDate { get; set; }
+        [StringLength(20)]
+        public string Status { get; set; }
+    }
+}
---
+++ todoapi/todoapi/Model/toDoContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace todoapi.Model
+{
+    public partial class toDoContext : DbContext
+    {
+        public toDoContext()
+        {
+        }
+
+        public toDoContext(DbContextOptions<toDoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Task> Task { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:nzmsatodo.database.windows.net,1433;Initial Catalog=todoDb;Persist Security Info=False;User ID=atea;Password=Master123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+
+            modelBuilder.Entity<Task>(entity =>
+            {
+                entity.Property(e => e.Status).IsUnicode(false);
+
+                entity.Property(e => e.TaskName).IsUnicode(false);
+            });
+        }
+    }
+}
